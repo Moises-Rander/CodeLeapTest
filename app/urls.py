72a0999939989c9d15b0b9e230013b4d178ea212
Moises Rander <moisesrander@outlook.com>
2025-01-08 03:58:15 -0300

    config admin

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,11 @@
+from django.contrib import admin
 from django.urls import path
 from posts.views import PostCreateListView, PostDetailUpdateDeleteView
 
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+
     path('posts/', PostCreateListView.as_view(), name='post-create-list'),
     path('posts/<int:pk>/', PostDetailUpdateDeleteView.as_view(), name='post-detail-update-delete'),
 ]
